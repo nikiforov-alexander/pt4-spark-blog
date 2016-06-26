7a24319b26f8c1fce6b47e41030b93f6c0e66229
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-06-26 18:33:19 +0200

    Added Before and BeforeClass Annotations, setUp and member variables for MainTest

--- src/test/java/com/teamtreehouse/blog/MainTest.java
+++ src/test/java/com/teamtreehouse/blog/MainTest.java
@@ -1,19 +1,34 @@
 package com.teamtreehouse.blog;
 
+import com.teamtreehouse.blog.dao.SimpleBlogEntryDAO;
+import com.teamtreehouse.blog.testing.ApiClient;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import spark.Spark;
 
 import static org.junit.Assert.*;
 
 public class MainTest {
-    @Before
-    public void setUp() throws Exception {
+    public static final String PORT = "4568";
+    private ApiClient mApiClient;
+    private SimpleBlogEntryDAO mSimpleBlogEntryDAO;
 
+    @BeforeClass
+    public static void startServer() {
+        String[] args = {PORT};
+        Main.main(args);
+    }
+    @AfterClass
+    public static void stopServer() {
+        Spark.stop();
     }
 
-    @After
-    public void tearDown() throws Exception {
-
+    @Before
+    public void setUp() throws Exception {
+        mSimpleBlogEntryDAO = new SimpleBlogEntryDAO();
+        mApiClient = new ApiClient("http://localhost:" + PORT);
     }
 
 }

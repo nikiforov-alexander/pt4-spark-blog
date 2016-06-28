d4d5302e0ce76b4e273b122a4c3be228001ae363
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-06-28 17:02:37 +0200

    Commented out the test blog entry added to simpleBlogEntryDAO, for testing purposes

--- src/test/java/com/teamtreehouse/blog/MainTest.java
+++ src/test/java/com/teamtreehouse/blog/MainTest.java
@@ -2,11 +2,13 @@ package com.teamtreehouse.blog;
 
 import com.teamtreehouse.blog.dao.SimpleBlogEntryDAO;
 import com.teamtreehouse.blog.testing.ApiClient;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import com.teamtreehouse.blog.testing.ApiResponse;
+import org.junit.*;
+import spark.ModelAndView;
 import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -31,4 +33,56 @@ public class MainTest {
         mApiClient = new ApiClient("http://localhost:" + PORT);
     }
 
+    @Test
+    public void emptyDaoPageIsTheSameAsModeled() throws Exception {
+//        BlogEntry blogEntry = new BlogEntry("title", "body");
+//        mSimpleBlogEntryDAO.addEntry(blogEntry);
+//        Map<String, Object> model = new HashMap<>();
+//        model.put("entries",mSimpleBlogEntryDAO.findAllEntries());
+        HandlebarsTemplateEngine handlebarsTemplateEngine =
+                new HandlebarsTemplateEngine();
+        String indexHtml = handlebarsTemplateEngine
+                .render(new ModelAndView(null, "index.hbs"));
+//        Document document = Jsoup.parse(indexHtml);
+//        Elements links = document
+//                .getElementsByAttributeValueStarting("href", "/entries/detail");
+//        String titleOfNewEntry = links.text();
+        ApiResponse apiResponse =
+                mApiClient.request("GET", "/");
+        assertEquals(indexHtml, apiResponse.getBody());
+    }
+
+    private String getHtmlOfPageWithHbsWithNullModel(String hbsFileName) {
+        HandlebarsTemplateEngine handlebarsTemplateEngine =
+                new HandlebarsTemplateEngine();
+        return handlebarsTemplateEngine
+                .render(new ModelAndView(null, hbsFileName));
+    }
+    private String getHtmlOfUnathorisedRequestToPage(String pageUri) {
+        ApiResponse apiResponse =
+                mApiClient.request("GET", "/entries/new");
+        return apiResponse.getBody();
+    }
+
+    @Test
+    public void unauthorisedRequestOnNewEntryPageRedirectsToPasswordPage()
+        throws Exception {
+        // Given no cookies with password
+        // When get to new entries page
+        // password html page should come as a response
+        assertEquals(
+                getHtmlOfPageWithHbsWithNullModel("password.hbs"),
+                getHtmlOfUnathorisedRequestToPage("/entries/new"));
+    }
+    @Test
+    public void unauthorisedRequestOnEditEntryPageRedirectsToPasswordPage()
+            throws Exception {
+        // Given no cookies with password
+        // When get to new entries page
+        // Then password html page should come as a response, even if there are
+        // no such entry
+        assertEquals(
+                getHtmlOfPageWithHbsWithNullModel("password.hbs"),
+                getHtmlOfUnathorisedRequestToPage("/entries/edit/somePage"));
+    }
 }

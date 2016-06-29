645a654f6ffca290a04c2c68a6683bd8ef3601fa
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-06-29 16:12:48 +0200

    added maximum tests that I could. for more I need better model of ApiClient, or ability to set query params of POST request

--- src/test/java/com/teamtreehouse/blog/MainTest.java
+++ src/test/java/com/teamtreehouse/blog/MainTest.java
@@ -19,7 +19,6 @@ public class MainTest {
     public static final String PORT = "4568";
     private static final String notFoundMessage = "No such entry found";
     private ApiClient mApiClient;
-    private SimpleBlogEntryDAO mSimpleBlogEntryDAO;
     private static final String mCookieWithPassword = "password=admin";
 
     @BeforeClass
@@ -34,24 +33,15 @@ public class MainTest {
 
     @Before
     public void setUp() throws Exception {
-        mSimpleBlogEntryDAO = new SimpleBlogEntryDAO();
         mApiClient = new ApiClient("http://localhost:" + PORT);
     }
 
     @Test
     public void emptyDaoPageIsTheSameAsModeled() throws Exception {
-//        BlogEntry blogEntry = new BlogEntry("title", "body");
-//        mSimpleBlogEntryDAO.addEntry(blogEntry);
-//        Map<String, Object> model = new HashMap<>();
-//        model.put("entries",mSimpleBlogEntryDAO.findAllEntries());
         HandlebarsTemplateEngine handlebarsTemplateEngine =
                 new HandlebarsTemplateEngine();
         String indexHtml = handlebarsTemplateEngine
                 .render(new ModelAndView(null, "index.hbs"));
-//        Document document = Jsoup.parse(indexHtml);
-//        Elements links = document
-//                .getElementsByAttributeValueStarting("href", "/entries/detail");
-//        String titleOfNewEntry = links.text();
         ApiResponse apiResponse =
                 mApiClient.request("GET", "/");
         assertEquals(indexHtml, apiResponse.getBody());
@@ -80,7 +70,6 @@ public class MainTest {
         return handlebarsTemplateEngine
                 .render(new ModelAndView(model, hbsFileName));
     }
-//    throw new ApiError(404, "No such entry found");
 
     @Test
     public void unauthorisedRequestOnNewEntryPageRedirectsToPasswordPage()

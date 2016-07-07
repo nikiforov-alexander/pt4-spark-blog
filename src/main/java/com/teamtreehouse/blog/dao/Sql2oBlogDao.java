461d52759fe9169aecf8e6dafdc55264b28d652c
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-07-07 12:52:44 +0200

    refactored class to be able to fetch entries from database

--- src/main/java/com/teamtreehouse/blog/dao/Sql2oBlogDao.java
+++ src/main/java/com/teamtreehouse/blog/dao/Sql2oBlogDao.java
@@ -1,36 +1,53 @@
 package com.teamtreehouse.blog.dao;
 
+import com.teamtreehouse.blog.exception.DaoException;
 import com.teamtreehouse.blog.model.BlogEntry;
-import com.teamtreehouse.blog.exception.NotFoundException;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Sql2oBlogDao implements BlogDao {
-    private List<BlogEntry> mBlogEntries;
+    // our database object
+    private final Sql2o mSql2o;
 
-    public Sql2oBlogDao() {
-       mBlogEntries = new ArrayList<>();
+    // default constructor
+    public Sql2oBlogDao(Sql2o sql2o) {
+        this.mSql2o = sql2o;
     }
 
     @Override
-    public boolean addEntry(BlogEntry blogEntry) {
-        return mBlogEntries.add(blogEntry);
+    public void addEntry(BlogEntry blogEntry) throws DaoException {
+        String sqlQuery
+                = "INSERT INTO entries(title, body) VALUES (:title, :body)";
+        try (Connection connection = mSql2o.open()) {
+            int id = (int) connection.createQuery(sqlQuery)
+                    .bind(blogEntry)
+                    .executeUpdate()
+                    .getKey();
+            blogEntry.setId(id);
+        } catch (Sql2oException sql2oException) {
+            throw new DaoException(sql2oException, "Problem adding entry");
+        }
     }
 
     @Override
     public List<BlogEntry> findAllEntries() {
-        return new ArrayList<>(mBlogEntries);
+        String sqlQuery = "SELECT * FROM entries";
+        try (Connection connection = mSql2o.open()) {
+            return connection.createQuery(sqlQuery)
+                    .executeAndFetch(BlogEntry.class);
+        }
     }
 
     @Override
-    public BlogEntry findEntryByHashId(String hashId)
-            throws NotFoundException {
-        return null;
-    }
-
-    @Override
-    public boolean removeEntry(BlogEntry blogEntry) {
-        return mBlogEntries.remove(blogEntry);
+    public BlogEntry findEntryById(int id) {
+        String sqlQuery = "SELECT * FROM entries WHERE id = :id";
+        try (Connection connection = mSql2o.open()) {
+            return connection.createQuery(sqlQuery)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(BlogEntry.class);
+        }
     }
 }
